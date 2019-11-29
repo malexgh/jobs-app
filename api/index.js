b6c9d3866ad67eeee6f637745b20754b7b49d5bb
Marcio <alexandrino.marcio@gmail.com>
2019-11-29 12:14:23 -0300

    added node rest api

---
+++ api/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const redisClient = require('redis').createClient();
+const { promisify } = require('util');
+
+const port = 3333;
+const getAsync = promisify(redisClient.get).bind(redisClient);
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.get('/jobs', async (req, res) => {
+    const jobs = await getAsync('github');
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); //CORS
+    res.send(jobs);
+});
+
+app.listen(port, () =>
+    console.log(`Server running. http://localhost:${port}`)
+);
